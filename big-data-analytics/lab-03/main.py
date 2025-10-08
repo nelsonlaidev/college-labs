98bab74bb13ea59046c8af8d2d6143561a819447
Nelson Lai <me@nelsonlai.dev>
2025-10-08 14:29:56 +0800

    feat: big data lab 3

---
+++ big-data-analytics/lab-03/main.py
@@ -0,0 +1,13 @@
+# fmt: off
+
+age = 19                                                         # integer
+height = 1.8                                                     # float
+name = "Nelson"                                                  # string
+is_student = True                                                # boolean
+programming_languages = ["Python", "JavaScript", "C++", "Rust"]  # list
+favorite_food = ("Apple", "Watermelon")                          # tuple
+hobbies = {"Coding", "Playing basketball"}                       # set
+address = {
+    "city": "New Territories",
+    "district": "Tin Shui Wai",
+}                                                                # dict
